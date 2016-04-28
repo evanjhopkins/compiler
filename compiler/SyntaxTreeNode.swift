be22c3b1e724ceefc61da8dbef5cfe5c5ea41fc7
evanjhopkins <evan@indiewalls.com>
2016-04-28 10:26:28 -0400

    multiple class improvements: isLeaf no longer needs to be explicitely stated. No start node required on init. etc..

--- compiler/SyntaxTreeNode.swift
+++ compiler/SyntaxTreeNode.swift
@@ -12,21 +12,28 @@ import Foundation
 class SyntaxTreeNode {
     var parent: SyntaxTreeNode?
     //let token: Token
-    var value: String
+    var value: String?
     var children: [SyntaxTreeNode]
     var isLeaf: Bool
     //var type: TokenType?
     
     //init node with parent
-    init(value: String, parent: SyntaxTreeNode?, isLeaf: Bool) {
-        self.value = value
+//    init(value: String, parent: SyntaxTreeNode?, isLeaf: Bool) {
+//        self.value = value
+//        self.children = []
+//        self.isLeaf = isLeaf
+//    }
+    
+    init() {
         self.children = []
-        self.isLeaf = isLeaf
+        self.isLeaf = true
     }
+
     
     //init node without parent
-    convenience init(value: String,isLeaf: Bool) {
-        self.init(value: value, parent: nil, isLeaf: isLeaf)
+    convenience init(value: String) {
+        self.init()
+        self.value = value
     }
 
     func setParent(parent: SyntaxTreeNode) {
@@ -35,20 +42,28 @@ class SyntaxTreeNode {
     }
     
     func addChild(child: SyntaxTreeNode) {
+        self.isLeaf = false
         child.parent = self
         self.children.append(child)
     }
     
     //add a child and decend into that child
     func addNode(value: String) -> SyntaxTreeNode {
-        let newNode = SyntaxTreeNode(value: value, isLeaf: false)
+        if (self.value == nil) {
+            self.value = value
+            return self
+        }
+        self.isLeaf = false
+        
+        let newNode = SyntaxTreeNode(value: value)
         newNode.setParent(self)
         return newNode
     }
     
     //add a child, but do not decend into that child for it is an only child with no children
     func addLeaf(value: String) {
-        let newNode = SyntaxTreeNode(value: value, isLeaf: true)
+        self.isLeaf = false
+        let newNode = SyntaxTreeNode(value: value)
         //newNode.type = type
         newNode.setParent(self)
     }
@@ -62,9 +77,9 @@ class SyntaxTreeNode {
         
         var wrappedValue: String
         if self.isLeaf {
-            wrappedValue = "[ "+self.value+" ]"
+            wrappedValue = "[ "+self.value!+" ]"
         }else {
-            wrappedValue = "< "+self.value+" >"
+            wrappedValue = "< "+self.value!+" >"
         }
         print(spacer + wrappedValue)
         for child in self.children {
